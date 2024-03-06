a78e52fc09e174b646a6d3e82da10f24c3d0837f
Karthik Ch <saipavan39dh@gmail.com>
2024-03-06 12:13:16 +0530

    Git Hub Url Updated

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const Orders = () => {
+  // Git Hub Url Updated
   return (
-    <div className='container-xxl'></div>
+    <div className='container-xxl'>
+        <div className="row">
+            <div className="col-12">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
